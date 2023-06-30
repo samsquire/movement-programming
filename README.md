6e556ca85e1d54f844d88729cb3b6d74dcac9e6e
samsquire <sam@samsquire.com>
2023-06-30 10:17:28 +0000

    Render JSON to table

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is an exploratory programming technique which is related to moving things a
 
 The idea is that complicated algorithms can be inferred by the patterns of movements around but even untrained people can learn where to move things to get the results they want.
 
-We can use puzzles and human intelligence to solve complicated problems.
+We can use puzzles and human intelligence to solve complicated problems.  
--- script.js
+++ script.js
@@ -1,4 +1,162 @@
-var td = document.querySelectorAll("table td");
+var td = document.querySelectorAll("#one table td");
+var two = document.querySelectorAll("#two table td");
+var doc = JSON.stringify({
+  "children": [
+    {"name": "bob", "age": 26},
+    {"name": "sam", "age": 33}
+  ]
+});
+console.log(doc);
+var pos = -1;
+var x = 0;
+var y = 0;
+var counter1 = 1000;
+var counter2 = 1000;
+var waitingForIdentifier = false;
+var type;
+var symbol = " ";
+var end = false;
+var last_char = " ";
+function getChar() {
+  console.log("pos", pos);
+  if (pos + 1 >= doc.length) {
+    end = true;
+    return doc[pos];
+  } else {
+    pos = pos + 1;
+    
+    return doc[pos];
+  }
+  
+}
+function getToken() {
+  type = "token";
+  symbol = last_char;
+  while (last_char == " ") {
+    last_char = getChar();
+  }
+  if (last_char == ":") {
+    last_char = getChar();
+    return "colon";
+  }
+  if (last_char == ",") {
+    last_char = getChar();
+    return "comma";
+  }
+  if (last_char == "\"") {
+    last_char = getChar();
+    return "quote";
+  }
+  if (last_char == "[") {
+    symbol = last_char
+    last_char = getChar();
+    return "opensquare";
+  }
+  if (last_char == "]") {
+    symbol = last_char;
+    last_char = getChar();
+    return "closesquare";
+  }
+  if (last_char == "{") {
+    symbol = last_char;
+    last_char = getChar();
+    
+    return "opencurly";
+  }
+  if (last_char == "}") {
+    symbol = last_char;
+    last_char = getChar();
+  
+    return "closecurly";
+  }
+  
+  const regex = /[A-Za-z0-9_]/g;
+  console.log(pos);
+
+  var found = last_char.match(regex);
+  var identifier = last_char;
+  if (found) {
+    
+    while (found && !end && counter2 > 0) {
+      last_char = getChar();
+      found = last_char.match(regex);
+      
+      console.log(last_char);
+      if (found) {
+        identifier += last_char; 
+      }
+      counter2--;
+    }
+    type = "identifier";
+    return identifier;
+  }
+}
+var stack = [];
+while (end == false && counter1 > 0) {
+  counter1--;
+  var token = getToken();
+  var output = token;
+  console.log(token);
+  if (token == "quote") {
+    continue;
+  }
+
+   if (token == "closecurly" || token == "closesquare" || token == "closecurly" || token == "opencurly" || token == "opensquare" ) {
+    output = symbol;
+   }
+  if (token == "closecurly" || token == "closesquare") {
+    output = symbol;
+    // y++;
+     // x -= 1;
+  }
+    if (token == "opencurly" || token == "opensquare") {
+      console.log("appending x", x)
+    stack.push(x);
+  }
+    if (token == "closecurly" || token == "closesquare") {
+    x = stack.pop();
+    y++;
+  }
+
+  if (token != "quote" && token != "colon" && token != "comma") {
+    $(two[(y * 10) + x]).html(output);
+  }
+  
+  
+   
+  if (token == "colon") {
+    waitingForIdentifier = true;
+
+  } 
+
+  if (waitingForIdentifier && type == "identifier") {
+    // y++;
+    // x -= 1;
+    waitingForIdentifier = false;
+  }
+  if (!waitingForIdentifier && type == "identifier") {
+    x += 1;
+  }
+
+  if (token == "opencurly" || token == "opensquare") {
+    y += 1;
+    x++;
+  }
+
+  if (token == "comma") {
+    y += 1;
+    x = stack[stack.length - 1] + 1;
+    // x -= 2;
+
+    
+  }
+  
+
+
+
+}
+console.log("opened");
+
 var picked = {waitingForEnd: false, gotLoopEnd: false};
 for (var x = 0 ; x < td.length; x++) {
   var row = Math.floor(x / 10);
